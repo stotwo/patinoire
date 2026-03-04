4db2665f2271575687cc6ce869801016b6c71853
dstl4 <141284777+stotwo@users.noreply.github.com>
2026-03-04 19:54:48 +0100

    fix: enable gh pages and setup basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/patinoire" : "",
   images: {
     unoptimized: true,
   },
